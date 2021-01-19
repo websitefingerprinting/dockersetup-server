553488fc52a447779639ce1577d6bf8344ef6d17
龚佳俊 <jgongac@connect.ust.hk>
2021-01-19 13:25:06 +0800

    change to clean mode

--- Entrypoint.sh
+++ Entrypoint.sh
@@ -5,7 +5,7 @@
 DEVICE=eth0
 BASE='/home/docker'
 TORRC_PATH='/home/docker/tunnel-proxy-linux-docker'
-PT='gan-tunnel'
+PT='trafficSniffer'
 # set offloads
 ifconfig ${DEVICE} mtu 1500
 ethtool -K ${DEVICE} tx off rx off tso off gso off gro off lro off
