02f87a359b397e830bae3369e6fa044da3a0afa6
龚佳俊 <jgongac@connect.ust.hk>
2021-01-30 12:10:18 +0800

    add rate limit config

--- Entrypoint.sh
+++ Entrypoint.sh
@@ -11,8 +11,9 @@ ifconfig ${DEVICE} mtu 1500
 ethtool -K ${DEVICE} tx off rx off tso off gso off gro off lro off
 pushd ${BASE}
 
-
 #set torrc file
+echo 'BandwidthRate 100 KBytes' >>  ${TORRC_PATH}
+echo 'BandwidthBurst 100 KBytes' >>  ${TORRC_PATH}
 echo 'DataDirectory /home/docker/tor-config/tunnel-proxy-hostport'$1 >> ${TORRC_PATH}
 echo 'Log notice stdout' >>  ${TORRC_PATH}
 echo 'SOCKSPort auto' >>  ${TORRC_PATH}
@@ -21,10 +22,12 @@ echo 'PublishServerDescriptor 0' >>  ${TORRC_PATH}
 echo 'Exitpolicy reject *:*' >>  ${TORRC_PATH}
 echo 'ORPort auto' >>  ${TORRC_PATH}
 echo 'ExtORPort auto' >>  ${TORRC_PATH}
-echo 'Nickname "tunnel" ' >>  ${TORRC_PATH}
+echo 'Nickname '${PT}'' >>  ${TORRC_PATH}
 echo 'BridgeRelay 1' >>  ${TORRC_PATH}
 echo 'ServerTransportListenAddr obfs4 0.0.0.0:35000' >>  ${TORRC_PATH}
 echo 'ServerTransportPlugin obfs4 exec /home/docker/'${PT}'/obfs4proxy/obfs4proxy' >>  ${TORRC_PATH}
 # echo 'ServerTransportPlugin obfs4 exec /home/docker/trafficSniffer/obfs4proxy/obfs4proxy' >> ${TORRC_PATH} 
+
+
 
 tor -f ${TORRC_PATH}
