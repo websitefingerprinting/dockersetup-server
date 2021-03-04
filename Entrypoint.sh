b8fd802b4262d75529405d9c1f3266acfb8f1ab1
龚佳俊 <jgongac@connect.ust.hk>
2021-03-04 17:44:43 +0800

    fix a typo

--- Entrypoint.sh
+++ Entrypoint.sh
@@ -14,7 +14,7 @@ ethtool -K ${DEVICE} tx off rx off tso off gso off gro off lro off
 
 # cp PT repository to container's own space.
 cp -r /home/docker/${PT} /home/
-pushd $home/{PT}
+pushd /home/{PT}
 go build -o obfs4proxy/obfs4proxy ./obfs4proxy
 
 pushd ${BASE}
